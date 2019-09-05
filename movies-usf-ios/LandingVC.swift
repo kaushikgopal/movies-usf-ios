dcd29fcec4c2c33f6c3883dc393d9ac0b8b21222
KG [motherbox-mini] <contact.github@kilogram.page>
2019-09-04 20:44:31 -0700

    [wip] create landing screen skeleton

---
+++ movies-usf-ios/LandingVC.swift
@@ -0,0 +1,31 @@
+//
+//  LandingVC.swift
+//  movies-usf-ios
+//
+//  Created by Kaushik Gopal on 9/4/19.
+//  Copyright © 2019 Kaushik Gopal. All rights reserved.
+//
+
+import UIKit
+
+class LandingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
