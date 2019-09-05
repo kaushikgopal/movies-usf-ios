8a8038748145ac65ec320a7e5612bfaab79afe71
KG [motherbox-mini] <contact.github@kilogram.page>
2019-09-04 20:40:57 -0700

    first commit

---
+++ movies-usf-iosTests/movies_usf_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  movies_usf_iosTests.swift
+//  movies-usf-iosTests
+//
+//  Created by Kaushik Gopal on 9/4/19.
+//  Copyright © 2019 Kaushik Gopal. All rights reserved.
+//
+
+import XCTest
+@testable import movies_usf_ios
+
+class movies_usf_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
