25b08ba27437119ee6d09071780b5dee25da5624
KG [kiMac] <contact@kilogram.page>
2019-09-11 16:20:45 -0700

    hide navigation bar

--- movies-usf-ios/AppDelegate.swift
+++ movies-usf-ios/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let navigationController = UINavigationController(rootViewController: LandingVC())
+        navigationController.setNavigationBarHidden(true, animated: false)
         
         window = UIWindow(frame: UIScreen.main.bounds) // fill screen
         window?.rootViewController = navigationController
