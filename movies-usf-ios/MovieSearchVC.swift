83d6de239ea796a53e9fb07fd772eb7fe5c5ab12
KG [MBP13] <contact.github@kilogram.page>
2019-09-15 18:36:36 -0700

    feat: add search result title view

--- movies-usf-ios/MovieSearchVC.swift
+++ movies-usf-ios/MovieSearchVC.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class MovieSearchVC: UIViewController {
 
-    let searchName = UITextField()
+    let sQuery = UITextField()
     let srImage = UIImageView()
+    let srTitle = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,7 @@ class MovieSearchVC: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // open with keyboard open
-        // searchName.becomeFirstResponder()
+        // sQuery.becomeFirstResponder()
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,38 +35,52 @@ class MovieSearchVC: UIViewController {
 
     private func setupUI() {
 
-        // setup search text
-        searchName.attributedPlaceholder = NSAttributedString(
+        // setup search query view
+        sQuery.attributedPlaceholder = NSAttributedString(
             string: "Search a Movie...",
             attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray]
         )
-        searchName.returnKeyType = UIReturnKeyType.search
-        searchName.textColor = .white
-        searchName.font = UIFont.boldSystemFont(ofSize: 28)
+        sQuery.returnKeyType = UIReturnKeyType.search
+        sQuery.textColor = .white
+        sQuery.font = UIFont.boldSystemFont(ofSize: 28)
 
-        searchName.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(searchName)
+        sQuery.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(sQuery)
         NSLayoutConstraint.activate([
-            searchName.topAnchor.constraint(equalTo: view.topAnchor, constant: 64),
-            searchName.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            searchName.heightAnchor.constraint(equalToConstant: 64)
+            sQuery.topAnchor.constraint(equalTo: view.topAnchor, constant: 64),
+            sQuery.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            sQuery.heightAnchor.constraint(equalToConstant: 64)
         ])
 
-        // setup search result
-        srImage.backgroundColor = .red
+        // setup search result image
+        srImage.backgroundColor = .darkGray
         srImage.contentMode = .scaleAspectFit
 
         srImage.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(srImage)
         NSLayoutConstraint.activate([
-            srImage.topAnchor.constraint(equalTo: searchName.bottomAnchor, constant: 12),
+            srImage.topAnchor.constraint(equalTo: sQuery.bottomAnchor, constant: 12),
             srImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
             srImage.heightAnchor.constraint(equalToConstant: 192),
             srImage.widthAnchor.constraint(equalToConstant: 128)
         ])
 
-//        let imageURL = URL(string: "https://i.ytimg.com/vi/07So_lJQyqw/maxresdefault.jpg")!
-//        srImage.load(url: imageURL)
+        // let imageURL = URL(string: "https://i.ytimg.com/vi/07So_lJQyqw/maxresdefault.jpg")!
+        // srImage.load(url: imageURL)
+
+        // setup search result name
+        srTitle.textColor = .white
+        srTitle.font = UIFont.boldSystemFont(ofSize: 22)
+
+        srTitle.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(srTitle)
+        NSLayoutConstraint.activate([
+            srTitle.topAnchor.constraint(equalTo: srImage.topAnchor),
+            srTitle.leadingAnchor.constraint(equalTo: srImage.trailingAnchor, constant: 24),
+            srTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
+        ])
+
+        // srTitle.text = "Movie Title"
     }
 }
 
